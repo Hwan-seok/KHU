3ac172eeab1dc5faa06154dc29c29ee814e57ed4
Heesu <huisu0715@khu.ac.kr>
2018-12-05 22:33:59 +0900

    mysql 개발 환경 구축 및 생활 기상 지수/사망확률 변수 정의

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host : 'ryulth.com',
+  user : 'kkonzi',
+  password : 'kkonzi1234',
+  database : 'kkonzi_test'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // mysql 세션 연결
+  connection.connect();
+  // connection.query('SELECT * FROM ');
+
+  // 자외선지수, 불쾌지수, 열지수, 체감 온도, 바람속도, 현재기온, 하늘 상태, 강우량, 태풍, 낙뢰
+  var uvr, discomfort, heat, sensibleTem, windSpeed, presentTem, skyState, rainfall, typhoon, lightning;
+  // 사망 확률
+  var deathProb;
+
+  deathProb = uvr + discomfort + heat + sensibleTem + windSpeed + presentTem + skyState + rainfall + typhoon + lightning;
+  res.render('index', {title: 'Express'});
+
+  connecttion.end();
 });
 
 module.exports = router;
