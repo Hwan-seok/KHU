0b49b6f3dd1360aac1ce601c71ab6078031dcfe4
Heesu <huisu0715@khu.ac.kr>
2018-12-06 12:07:19 +0900

    MySQL 세션 파트 삭제

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,9 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host : 'ryulth.com',
-  user : 'kkonzi',
-  password : 'kkonzi1234',
-  database : 'kkonzi_test'
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // mysql 세션 연결
-  connection.connect();
+
   // connection.query('SELECT * FROM ');
 
   // 자외선지수, 불쾌지수, 열지수, 체감 온도, 바람속도, 현재기온, 하늘 상태, 강우량, 태풍, 낙뢰
@@ -22,8 +14,6 @@ router.get('/', function(req, res, next) {
 
   deathProb = uvr + discomfort + heat + sensibleTem + windSpeed + presentTem + skyState + rainfall + typhoon + lightning;
   res.render('index', {title: 'Express'});
-
-  connecttion.end();
 });
 
 module.exports = router;
