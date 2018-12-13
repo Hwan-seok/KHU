7882825a900b5cfb3cffed325b986cb6bca00687
KKonzi <whalswl97@khu.ac.kr>
2018-12-14 00:34:45 +0900

    first try

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'code')));
+app.use('/name/:name/birth',express.static(path.join(__dirname, 'public')));
 app.use('/name/:name/birth',express.static(path.join(__dirname, 'code')));
 
 app.use('/', indexRouter);
