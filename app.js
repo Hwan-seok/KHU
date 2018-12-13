036e2a64e2ff0dc7cf6c86efdef5d53c95cee2b6
KKonzi <whalswl97@khu.ac.kr>
2018-12-14 02:46:41 +0900

    ejs errrorrrrrr

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'code')));
+app.use('/',express.static(path.join(__dirname, 'public')));
+app.use('/',express.static(path.join(__dirname, 'code')));
 app.use('/name/:name/birth',express.static(path.join(__dirname, 'public')));
 app.use('/name/:name/birth',express.static(path.join(__dirname, 'code')));
 
